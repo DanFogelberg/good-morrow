02d6ce2341baf89da7c7aca44b1ae5df1c1c8b09
DanFogelberg <dan.f.fogelberg@gmail.com>
2022-12-20 18:07:35 +0100

    Made .env work

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 require "hotelFunctions.php";
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use Dotenv\Dotenv;
+
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+echo $_ENV["USER_NAME"];
+
 ?>
 
 <!DOCTYPE html>
