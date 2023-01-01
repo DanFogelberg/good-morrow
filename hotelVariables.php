c5a60e3542719e37c2b83ef141c14088763528fa
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-01-01 17:46:30 +0100

    Made room data belong to .txt file

--- hotelVariables.php
+++ hotelVariables.php
@@ -1,15 +1,7 @@
 <?php
 
-$rooms = [
-                    "basic" => ["cost" => 4, "roomNumber" => 1],
-                    "average" => ["cost" => 6, "roomNumber" => 2],
-                    "high" => ["cost" => 8, "roomNumber" => 3]
-];
+$rooms = json_decode(file_get_contents(__DIR__ . "/hotelVariables.txt"), true);
 
-//Prices for hotel rooms //Legacy
-$basicPrice = 4;
-$averagePrice = 6;
-$highPrice = 8;
 
 //Stars of hotel
 $stars = 1;
